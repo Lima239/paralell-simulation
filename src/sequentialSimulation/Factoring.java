ee691c04166c5c95ebca8edf40385c3d543fc5d4
Lima239 <j.lichmanova@gmail.com>
2021-11-30 22:46:12 +0100

    uploads

--- src/sequentialSimulation/Factoring.java
+++ src/sequentialSimulation/Factoring.java
@@ -1,6 +1,27 @@
 package sequentialSimulation;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Factoring implements Algorithms{
+    private ArrayList<Double> tasks;
+    private Iterator<Double> iteratorOfTasks;
+    private int numberOfProcessors;
+    private double latency;
+    private ArrayList<Processor> processors;
+
+    public Factoring(ArrayList<Double> tasks, int numberOfProcessors, double latency){
+        this.tasks = tasks;
+        this.numberOfProcessors = numberOfProcessors;
+        this.latency = latency;
+        this.iteratorOfTasks =  tasks.iterator();
+
+        processors = new ArrayList<>();
+        for (int i = 1; i < numberOfProcessors; i++){
+            Processor processor = new Processor(this.latency);
+            processors.add(processor);
+        }
+    }
 
     @Override
     public void redistributeTasks() {
--- src/sequentialSimulation/Processor.java
+++ src/sequentialSimulation/Processor.java
@@ -3,24 +3,18 @@ package sequentialSimulation;
 import java.util.*;
 
 public class Processor {
-    private boolean idle;
     private double timeCounter;
-    private final int latency;
+    private final double latency;
     private ArrayList<Double> listOfTasks;
     private ArrayList<ArrayList<Double>> listOfJobs;
-    private ArrayList<Double> historyOfTasks;
-    private ArrayList<ArrayList<Double>> historyOfJobs;
     private Iterator<Double> iteratorOfTasks;
     private Iterator<ArrayList<Double>> iteratorOfJobs;
 
-    public Processor(int latency){
-        this.idle = false;
+    public Processor(double latency){
         this.timeCounter = 0;
         this.latency = latency;
         this.listOfTasks = new ArrayList<>();
         this.listOfJobs = new ArrayList<>();
-        this.historyOfTasks = new ArrayList<>();
-        this.historyOfJobs = new ArrayList<>();
         this.iteratorOfTasks =  listOfTasks.iterator();
         this.iteratorOfJobs = listOfJobs.iterator();
     }
@@ -28,27 +22,22 @@ public class Processor {
     public void reset(){
         this.listOfTasks.clear();
         this.listOfJobs.clear();
-        this.historyOfTasks.clear();
-        this.historyOfJobs.clear();
     }
 
     public void read_job(ArrayList<Double> listOfTasks){
+        timeCounter += latency;
         listOfJobs.add(listOfTasks);
     }
 
     public double execute(){
         if(iteratorOfTasks.hasNext()){
             double nextTask = iteratorOfTasks.next();
-            historyOfTasks.add(nextTask);
-            timeCounter += latency + nextTask;
+            timeCounter += nextTask;
             return timeCounter;
         } else if(iteratorOfJobs.hasNext()){
-            this.historyOfJobs.add(this.listOfTasks);
-            this.historyOfTasks = new ArrayList<>();
             this.listOfTasks = new ArrayList<>(iteratorOfJobs.next());
             if(iteratorOfTasks.hasNext()){
                 double nextTask = iteratorOfTasks.next();
-                historyOfTasks.add(nextTask);
                 timeCounter += latency + nextTask;
                 return timeCounter;
             } else {
@@ -67,9 +56,4 @@ public class Processor {
         } catch(NoSuchElementException e){}
         return result;
     }
-
-    public ArrayList HistoryOfTasks(){
-        return this.historyOfTasks;
-    }
-
 }
