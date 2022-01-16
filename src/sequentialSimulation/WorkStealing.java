4eaef21c80d7510c958692f46734d817f27678d9
Lima239 <j.lichmanova@gmail.com>
2022-01-16 20:26:18 +0100

    implementation of constructor

--- src/sequentialSimulation/WorkStealing.java
+++ src/sequentialSimulation/WorkStealing.java
@@ -1,6 +1,29 @@
 package sequentialSimulation;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class WorkStealing implements Algorithms{
+    private ArrayList<Double> tasks;
+    private Iterator<Double> iteratorOfTasks;
+    private int numberOfProcessors;
+    private double latency;
+    private ArrayList<Processor> processors;
+    private int numberOfTasks;
+
+    public WorkStealing(ArrayList<Double> tasks, int numberOfProcessors, double latency){
+        this.tasks = tasks;
+        this.numberOfProcessors = numberOfProcessors;
+        this.latency = latency;
+        this.iteratorOfTasks =  tasks.iterator();
+        this.numberOfTasks = tasks.size();
+
+        processors = new ArrayList<>();
+        for (int i = 1; i < numberOfProcessors; i++){
+            Processor processor = new Processor(this.latency);
+            processors.add(processor);
+        }
+    }
 
     @Override
     public void redistributeTasks() {
