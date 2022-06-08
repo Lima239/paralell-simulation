8a8d52cd44c6c889a67d515e38e754d94de9b69d
Lima239 <j.lichmanova@gmail.com>
2022-06-08 19:00:51 +0200

    iterator implementation

--- src/sequentialSimulation/JobsIterator.java
+++ src/sequentialSimulation/JobsIterator.java
@@ -1,2 +1,41 @@
-package sequentialSimulation;public class JobsIterator {
+package sequentialSimulation;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class JobsIterator implements Iterable<ArrayList<Double>> {
+    private ArrayList<ArrayList<Double>> jobs;
+
+    public JobsIterator(ArrayList<ArrayList<Double>> jobs){
+        this.jobs = jobs;
+    }
+
+    public void addJob(ArrayList<Double> job){
+        jobs.add(job);
+    }
+
+    @Override
+    public Iterator iterator() {
+        Iterator<ArrayList<Double>> it = new Iterator<>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < jobs.size();
+            }
+
+            @Override
+            public ArrayList<Double> next() {
+                if (this.hasNext()) {
+                    int tmpIndex = index;
+                    index++;
+                    return jobs.get(tmpIndex);
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
+        return it;
+    }
 }
