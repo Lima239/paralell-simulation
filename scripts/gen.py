1f397b8f2ed14d2c265f4de5676df595b9f9fc64
Lima239 <j.lichmanova@gmail.com>
2022-06-22 18:15:34 +0200

    with command line arguments

--- scripts/gen.py
+++ scripts/gen.py
@@ -1,5 +1,7 @@
 import numpy as np
+import sys
 
+# command line arguments MIN MAX AVG W
 def gen(t_min, t_max, target_avg, n, tolerance=0.000001):
     r = np.array([t_min, t_max])
     l = np.random.default_rng().uniform(t_min, t_max, n)
@@ -10,7 +12,7 @@ def gen(t_min, t_max, target_avg, n, tolerance=0.000001):
         avg = np.average(concat)
         diff = abs(target_avg - avg) 
         if diff <= tolerance:
-            print(f'Number of iterations: {i}')
+            # print(f'Number of iterations: {i}')
             return concat
         l = l + (target_avg - avg)
         l = np.extract(l >= t_min, l)
@@ -21,15 +23,13 @@ def gen(t_min, t_max, target_avg, n, tolerance=0.000001):
             l_new = np.random.default_rng().uniform(avg, target_avg + diff, n - 2 - len(l))
         l = np.concatenate((l, l_new))
 
-MIN = 7
-MAX = 14
-AVG = 10
-W = 100
-FILENAME = '/Users/JuliaLichmanova/Desktop/paralell-simulation/input.txt'
+MIN = float(sys.argv[1])
+MAX = float(sys.argv[2])
+AVG = float(sys.argv[3])
+W = int(sys.argv[4])
 
 result = gen(MIN, MAX, AVG, W)
 
-print('Generated result with min: {0}, max: {1}, avg: {2} and length: {3}'.format(np.min(result), np.max(result), np.average(result), len(result)))
+#print('Generated result with min: {0}, max: {1}, avg: {2} and length: {3}'.format(np.min(result), np.max(result), np.average(result), len(result)))
 
-with open(FILENAME, 'w+') as f:
-    f.write('\n'.join([str(MIN), str(MAX), ' '.join(map(str, result))]))
+print('\n'.join([str(MIN), str(MAX), ' '.join(map(str, result))]))
