1b517c7ba4cdf81b3b29dfd482ea581c3402ee58
Lima239 <j.lichmanova@gmail.com>
2022-06-22 17:43:40 +0200

    with command line arguments and special algorithm input

--- src/sequentialSimulation/Simulator.java
+++ src/sequentialSimulation/Simulator.java
@@ -2,33 +2,91 @@ package sequentialSimulation;
 
 import java.util.*;
 import java.io.*;
+import java.util.Collections;
 
+// command line arguments NameOfInputTXT Algorithm(C/F/W) Latency N(number of processors) [Parameter(optional)]
 public class Simulator {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(new FileInputStream("input.txt"));
-        PrintStream out = new PrintStream("output.txt");
-        double latency = 0.3;
-        int parameter = 1;
-        float min = scanner.nextFloat();
-        float max = scanner.nextFloat();
-        ArrayList<Double> tasks = new ArrayList<>();
-        while (scanner.hasNext()){
-            tasks.add(scanner.nextDouble());
+        if (args.length > 0) {
+            Scanner scanner = new Scanner(new FileInputStream(args[0]));
+            String algorithm = args[1];
+            double latency = Double.parseDouble(args[2]);
+            int numberOfProcessors = Integer.parseInt(args[3]);
+            int parameter = Integer.parseInt(args[4]);
+
+            ArrayList<Double> tasks = new ArrayList<>();
+            while (scanner.hasNext()) {
+                tasks.add(scanner.nextDouble());
+            }
+
+            double avg = average(tasks);
+            double min = Collections.min(tasks);
+            double max = Collections.max(tasks);
+
+            switch(algorithm){
+                case "C":
+                    PrintStream outC = new PrintStream("outputChunking.txt");
+                    outC.println(tasks.size());
+                    outC.println(numberOfProcessors);
+                    outC.println(avg);
+                    outC.println(min);
+                    outC.println(max);
+                    outC.println(latency);
+                    for (int i = 1; i <= numberOfProcessors; i++) {
+                        Algorithms chunking = new Chunking(tasks, i, latency, parameter);
+                        outC.print(i + " ");
+                        outC.print(chunking.redistributeTasks() + " ");
+                        outC.println();
+                    }
+                    outC.close();
+                    break;
+
+                case "F":
+                    PrintStream outF = new PrintStream("outputFactoring.txt");
+                    outF.println(tasks.size());
+                    outF.println(numberOfProcessors);
+                    outF.println(avg);
+                    outF.println(min);
+                    outF.println(max);
+                    outF.println(latency);
+                    for (int i = 1; i <= numberOfProcessors; i++) {
+                        double factor = 1 + (max / min) * (i - 1);
+                        Algorithms factoring = new Factoring(tasks, i, latency, factor);
+                        outF.print(i + " ");
+                        outF.print(factoring.redistributeTasks() + " ");
+                        outF.println();
+                    }
+                    outF.close();
+                    break;
+
+                case "W":
+                    PrintStream outW = new PrintStream("outputWorkStealing.txt");
+                    outW.println(tasks.size());
+                    outW.println(numberOfProcessors);
+                    outW.println(avg);
+                    outW.println(min);
+                    outW.println(max);
+                    outW.println(latency);
+                    for (int i = 1; i <= numberOfProcessors; i++) {
+                        Algorithms workStealing = new WorkStealing(tasks, i, latency);
+                        outW.print(i + " ");
+                        outW.print(workStealing.redistributeTasks() + " ");
+                        outW.println();
+                    }
+                    outW.close();
+                    break;
+            }
+
+        } else {
+            System.out.println("No command line arguments found.");
         }
+    }
 
-        for (int i = 1; i <= 100; i++){
-            Algorithms chunking = new Chunking(tasks, i, latency, parameter);
-            Algorithms workStealing = new WorkStealing(tasks, i, latency);
-            float factor = 1 + (max / min) * (i - 1);
-            Algorithms factoring = new Factoring(tasks, i, latency, factor);
-
-            out.print(i + " ");
-            out.print(chunking.redistributeTasks() + " ");
-            out.print(workStealing.redistributeTasks() + " ");
-            out.print(factoring.redistributeTasks() + " ");
-            out.println();
+    static double average(ArrayList<Double> tasks){
+        double resultSum = 0;
+        for(Double task : tasks){
+            resultSum += task;
         }
-
-        out.close();
+        return resultSum/tasks.size();
     }
 }
